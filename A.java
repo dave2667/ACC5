386ffe024153611661e025e7ee615e1769ce76a6
Dave M <david.miller8@g.austincc.edu>
2017-04-19 16:40:15 -0500

    Add files via upload
    Day One code

---
+++ A.java
@@ -0,0 +1,19 @@
+public class A {
+  // Comments start with the double-slash
+  /*  Comment block
+      can be multi-line */
+  public static void main(String args) {
+
+       String x = "8";
+       int y = 8;
+
+       System.out.println("Hello world!  This is my first program in Java The quick " + 
+       "brown fox jumped over \nthe lazy dog.  Now is the time for all good men to " + 
+       "come to the aid of their \ncountry." + 
+       args + x + y);
+        }
+
+  public static void main(String[] args) {
+          main("Ditto");    
+        }
+      }
